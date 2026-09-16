1526fb75a84f555a71923521c397cea237477537
Zachary Ross <zross@academic.rrc.ca>
2026-09-16 15:32:07 -0500

    feature: fixed the first bug that made profitOrLoss being a divided value of initialInvestment and current value instead of subtracted of currentValue by initialInvestment.

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -1,12 +1,24 @@
 export function calculatePortfolioPerformance(): any {
+    /*current implementation is hardcoded,
+    will research to figure out how to allow the user/tester
+    to make up numbers and pass them through.
+    */
     let initialInvestment = 10000;
     let currentValue = 12000;
-
-    const profitOrLoss = initialInvestment / currentValue;
-
+   /*calculate the profit or loss
+    to be the current value minus the intial investment.*/
+    const profitOrLoss = currentValue - initialInvestment;
+    /* 
+    the current implementation may have a bug but am unsure,
+    will research the video to see.
+   */
     const percentageChange = (profitOrLoss / initialInvestment) * 100;
 
     let performanceSummary;
+    /*
+    current implementation is in violation of the instructions,
+    need to read the notes or research online to remove the if statement
+    */
     if (percentageChange > 20) {
         performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
     } else {
