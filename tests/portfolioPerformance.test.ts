af2182d05358b2c43ab61ac4d4cf760a06ca5849
Zachary Ross <zross@academic.rrc.ca>
2026-09-23 13:12:14 -0500

    features: added test to the package.json and added a test for a high performing portfolio.

--- tests/portfolioPerformance.test.ts
+++ tests/portfolioPerformance.test.ts
@@ -8,3 +8,33 @@ import app from "../src/app";
 
 // import testing
 import { it, expect, describe } from '@jest/globals';
+
+describe("GET /api/v1/portfolio/performance", () => {
+    it("should return portfolio performance summary for a high performing portfolio", async () => {
+        // create GET request to portfolio performance endpoint with query parameters
+        const response: Response = await request(app)
+            .get("/api/v1/portfolio/performance")
+            .query({ initialInvestment: 1000, currentValue: 1500 });
+
+        // assert response status OK and performance object to have specified properties
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("initialInvestment");
+        expect(response.body).toHaveProperty("currentValue");
+        expect(response.body).toHaveProperty("performance");
+    });
+});
+
+describe("GET /api/v1/portfolio/performance", () => {
+    it("should return portfolio performance summary for a no change portfolio", async () => {
+        // create GET request to portfolio performance endpoint with query parameters
+        const response: Response = await request(app)
+            .get("/api/v1/portfolio/performance")
+            .query({ initialInvestment: 1000, currentValue: 1000 });
+
+        // assert response status OK and performance object to have specified properties
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("initialInvestment");
+        expect(response.body).toHaveProperty("currentValue");
+        expect(response.body).toHaveProperty("performance");
+    });
+});
